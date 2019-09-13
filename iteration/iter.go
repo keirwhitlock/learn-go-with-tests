ce6b5e2dc45d749449ee3e10b306632da0835177
Keir Whitlock <keir@whitlock.it>
2019-09-13 16:56:50 +0100

    Added "U" flag to turn Repeat()'d output to upper case using strings.ToUpper().

--- iteration/iter.go
+++ iteration/iter.go
@@ -1,9 +1,14 @@
 package iteration
 
-func Repeat(character string, count int) string {
+import "strings"
+
+func Repeat(character string, count int, upper string) string {
 
 	var repeated string
 	for i := 0; i < count; i++ {
+		if upper == "U" {
+			character = strings.ToUpper(character)
+		}
 		repeated += character
 	}
 	return repeated
--- iteration/iter_test.go
+++ iteration/iter_test.go
@@ -6,22 +6,33 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a", 10)
-	expected := "aaaaaaaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+	t.Run("repeating a character n times", func(t *testing.T) {
+		repeated := Repeat("a", 10, "")
+		expected := "aaaaaaaaaa"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("change all characters to uppercase", func(t *testing.T) {
+		repeatedUpper := Repeat("k", 10, "U")
+		expected := "KKKKKKKKKK"
+
+		if repeatedUpper != expected {
+			t.Errorf("expected %q but got %q", expected, repeatedUpper)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a", 10)
+		Repeat("a", 10, "")
 	}
 }
 
 func ExampleRepeat() {
-	repeated := Repeat("a", 20)
+	repeated := Repeat("a", 20, "")
 	fmt.Println(repeated)
 	// Output: aaaaaaaaaaaaaaaaaaaa
 }
